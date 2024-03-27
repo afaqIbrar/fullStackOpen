cf09912c5ebe66727a233ac196e233455b9e1de2
afaqibrar <afaqibrar468@gmail.com>
2024-03-27 16:47:19 +0500

    part1-1.9-1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
-const Button = ({handleClick,text}) => {
+const Button = ({ handleClick, text }) => {
   return (
     <button onClick={handleClick}>{text}</button>
   );
 }
-const Statistic = ({value , text}) => {
-    return (
-      <>
+const StatisticLine = ({ value, text }) => {
+  return (
+    <>
       <tr>
         <td>{text}</td>
         <td>{value}</td>
       </tr>
-     </>
+    </>
+  )
+}
+
+const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return (
+      <div>No feedbak given</div>
     )
+  }
+  let total = good + bad + neutral;
+  let average = (good - bad) / total;
+  let positive = (good / total) + '%';
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
+  )
 }
 
 function App() {
@@ -34,17 +57,13 @@ function App() {
 
   return (
     <>
-     <h1>give feedback</h1>
-     <Button handleClick={handleGoodClick} text="good" />
-     <Button handleClick={handleNeutrallick} text="neutral" />
-     <Button handleClick={handleBadClick} text="bad" />
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutrallick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
 
-     <h1>Statistics</h1>
-     <Statistic text="good" value={good}/>
-     <Statistic text="neutral" value={neutral}/>
-     <Statistic text="bad" value={bad}/>
-     <Statistic text="average" value={(good - bad )/ good+bad+neutral + '%'}/>
-     <Statistic text="positive" value={good / good+bad+neutral + '%'}/>
+      <h1>Statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
 
   )
